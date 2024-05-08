81c6b2fb0289cc33b5ce350bd27e9db4ba9323c4
Darina Khalina <darina.khalina@namecheap.com>
2024-05-08 14:32:34 +0300

    chore(hw7): add task 1

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,11 @@
-console.log(1);
+const categories = document.querySelectorAll('ul#categories li.item');
+
+console.log(`Number of categories: ${categories.length}`);
+
+categories.forEach(categoryItem => {
+  const categoryName = categoryItem.querySelector('h2').textContent;
+  const categoryItems = categoryItem.querySelectorAll('ul li');
+
+  console.log(`Category: ${categoryName}`);
+  console.log(`Elements: ${categoryItems.length}`);
+});
