d1504fe6b66453cd5427ec35a5f5f14ceb00641e
Darina Khalina <darina.khalina@namecheap.com>
2024-05-08 15:31:48 +0300

    chore(hw7): add task 3

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,8 @@
-console.log(3);
+const input = document.querySelector('#name-input');
+const output = document.querySelector('#name-output');
+const defaultText = output.innerText;
+
+input.addEventListener('input', event => {
+  const inputValue = event.currentTarget.value.trim();
+  output.textContent = inputValue || defaultText;
+});
