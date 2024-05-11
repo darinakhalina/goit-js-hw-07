028fc0650ff880ac36fa97faf5e55024a4663c81
Darina Khalina <darina.khalina@namecheap.com>
2024-05-11 20:11:49 +0300

    chore(hw7): add task 5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector('body');
+const button = document.querySelector('.change-color');
+const colorName = document.querySelector('.color');
+
+const handleClick = () => {
+  const newColor = getRandomHexColor();
+  body.style.backgroundColor = newColor;
+  colorName.innerText = newColor;
+};
+
+button.addEventListener('click', handleClick);
