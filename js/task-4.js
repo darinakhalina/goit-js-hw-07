a406575aa2f24752175c12709a4bda6835fbad42
Darina Khalina <darina.khalina@namecheap.com>
2024-05-11 20:03:27 +0300

    chore(hw7): add task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,26 @@
-console.log(4);
+const loginForm = document.querySelector('.login-form');
+
+loginForm.addEventListener('submit', handleFormSubmit);
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  const { email, password } = event.currentTarget.elements;
+
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+
+  if (!emailValue || !passwordValue) {
+    alert('All form fields must be filled in');
+    return;
+  }
+
+  const formData = {
+    email: emailValue,
+    password: passwordValue,
+  };
+
+  console.log(formData);
+
+  loginForm.reset();
+}
